221fb635749ae88ecd16c360049d5e8f9916a203
Hindi Sahayata <79802622+hindisahayata@users.noreply.github.com>
2025-10-16 12:10:11 +0530

    Delete lazyadsense.js

--- lazyadsense.js
+++
@@ -1,6 +0,0 @@
-<script>//<![CDATA[
-/*!Lazy adsence 2025 by hindisahayata.in  | https://hindisahayata.in */
-function lazyAdLoad(){let e=document.createElement("script");e.setAttribute("crossorigin","anonymous"),e.async=!0,e.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8755951641490176",document.head.appendChild(e)}const lazyAdStorage=localStorage.getItem("lazyAd");if("true"===lazyAdStorage)lazyAdLoad();else{let e=!1,t=!1;function a(){e||0===document.documentElement.scrollTop&&0===document.body.scrollTop||(e=!0,t=!0,lazyAdLoad(),localStorage.setItem("lazyAd","true"))}window.addEventListener("scroll",a,{passive:!0}),window.addEventListener("click",()=>{t||(t=!0,e=!0,lazyAdLoad(),localStorage.setItem("lazyAd","true"))},{once:!0,passive:!0})}
-
-/*]]>*/
-</script>
